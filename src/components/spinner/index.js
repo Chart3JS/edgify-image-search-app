ac8eed77fe6f9d98b8a50a3f918a17bd10c83e70
Chart3JS <jprjdev@gmail.com>
2020-04-08 02:17:18 +0300

    remove unnecessary files and code

--- src/components/spinner/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styles from './spinner.css';
-
-const Spinner = () =>
-    <div className={styles.container}>
-        <img src='images/ajax-loader.gif' alt='Loader...'/>
-    </div>;
-
-export default Spinner; 
--- src/custom-types/search-types.js
+++ src/custom-types/search-types.js
@@ -4,8 +4,3 @@ export const SearchResult = PropTypes.shape({
     thumbnail: PropTypes.string.isRequired,
     full: PropTypes.string.isRequired,
 });
-
-export const Page = PropTypes.shape({
-    path: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-});
